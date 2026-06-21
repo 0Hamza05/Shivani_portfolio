c63bbf273c99940cff8d6d079b09d493ec967cd3
Hamza Tamboowala <htamboowala05@gmail.com>
2026-06-21 17:27:12 +0530

    Welcome page

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -47,7 +47,7 @@ export default function Welcome({ onDismiss }) {
 
   return (
     <motion.div
-      initial={{ opacity: 0 }}
+      initial={{ opacity: 1 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: shouldReduceMotion ? 0 : 1 }}
